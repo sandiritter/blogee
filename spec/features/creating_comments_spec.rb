52ca9dcb058386563e56a980cf642a70ba3070fa
Ben Stafford <bendstafford@gmail.com>
2014-04-18 19:41:44 -0400

    Edit specs

--- spec/features/creating_comments_spec.rb
+++ spec/features/creating_comments_spec.rb
@@ -9,7 +9,7 @@ feature "Creating Comments" do
 
   scenario "with valid attributes" do
     fill_in "Author", with: "Anonymous Internet User"
-    fill_in "Comment", with: "You're wrong! This is the Internet - whatever you say is wrong."
+    fill_in "Content", with: "You're wrong! This is the Internet - whatever you say is wrong."
     click_button "Add Comment"
     expect(current_path).to eq post_path(@post)
     expect(page).to have_content("Comment has been saved.")
@@ -17,9 +17,9 @@ feature "Creating Comments" do
 
   scenario "must have content longer than 15 characters" do
     fill_in "Author", with: "Anonymous Internet User"
-    fill_in "Comment", with: "You're wrong!"
+    fill_in "Content", with: "You're wrong!"
     click_button "Add Comment"
-    expect(page).to have_content("Your comment must be at least 15 characters long.")
+    expect(page).to have_content("Content must be at least 15 characters long.")
   end
 
   scenario "with invalid attributes" do
